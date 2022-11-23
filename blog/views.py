11eccdf1fec7a2607750ae329a2ae8d672ba5ce3
YasmineBB <y.bourhabi@gmail.com>
2022-11-23 10:29:25 +0000

    Update database url

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views import generic, View
 from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -52,6 +53,7 @@ class PostDetail(View):
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
+            messages.success(request, 'Your comment has been successfully posted!')
         else:
             comment_form = CommentForm()
         
